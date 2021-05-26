5c0f04d0a76905fe88410ae568b377f6685ab61c
Brendon <breezy@Brendons-MacBook-Air.local>
2021-05-26 11:10:52 -0400

    Navbar Completed

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div>
+      <Navbar/> 
+      <h1>Brendon's Official Portfolio!!</h1> 
+    </div>
+  );
+}
+
+export default App;
